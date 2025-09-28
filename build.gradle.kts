6fa88feb20edc388a915c300ca560904906ba150
shyFoo <oasis1975sun@gmail.com>
2025-09-28 13:24:27 +0900

    add Redis session management and update test configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,19 +19,22 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2025.0.0"
-
+extra["testcontainers.version"] = "1.21.3"
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.session:spring-session-data-redis:3.5.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.testcontainers:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainers.version")}")
 	}
 }
 
