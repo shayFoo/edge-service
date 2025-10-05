d8c33fa6514b0987598d2c61309299a5a6a057e3
shyFoo <oasis1975sun@gmail.com>
2025-10-05 21:45:49 +0900

    add SPA route configuration and enhance security filter chain

--- src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.polarbookshop.edge_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
@@ -16,7 +21,14 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http,
                                                       ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return http.authorizeExchange(exchange ->
-                        exchange.anyExchange().authenticated())
+                        exchange
+                                .pathMatchers(HttpMethod.GET, "/").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/*.css", "/*.js", "/favicon.ico").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                                .anyExchange().authenticated())
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(
+                        new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+                ))
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .oauth2Login(Customizer.withDefaults())
